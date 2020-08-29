7b6adb4447cbb5170d65f620f3586fd230f30d9e
carslan3455 <69872909+carslan3455@users.noreply.github.com>
2020-08-29 23:32:50 +0200

    ikinci class tamamlandi

---
+++ IlkCommitveYukleme/src/Ikinci_Yukleme.java
@@ -0,0 +1,10 @@
+public class Ikinci_Yukleme {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 10;
+
+        System.out.println(a*b);
+        System.out.println("Yedek branch olusturup yukleme yapiyoruz");
+    }
+}
