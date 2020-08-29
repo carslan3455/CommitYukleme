e077a3ad0a8cdf317bb3acd571438e586f8f7c60
carslan3455 <69872909+carslan3455@users.noreply.github.com>
2020-08-29 23:00:13 +0200

    Ilk class tamamlandi

---
+++ IlkCommitveYukleme/src/Ilk_Yukleme.java
@@ -0,0 +1,11 @@
+public class Ilk_Yukleme {
+    public static void main(String[] args) {
+
+        int a= 12;
+        int b= 123;
+
+        System.out.println(a+b);
+        System.out.println(""+a+b);
+
+    }
+}
