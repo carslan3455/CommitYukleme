64a08edd01195fc25f704a8947ce578d6d6f9e5f
Ahmet Reha Salmaner <rsalmaner@gmail.com>
2020-12-28 20:37:15 +0100

    TEST-123 mergeTest added.

---
+++ YeniProje/src/mergeTest.java
@@ -0,0 +1,36 @@
+public class mergeTest {
+
+    private String name;
+    private String surname;
+    private int number;
+
+    public mergeTest(String name, String surname, int number) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
